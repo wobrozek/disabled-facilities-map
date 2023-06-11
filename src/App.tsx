75045c0619d8fd64ed4849bc0027f0ca8f2a132b
lidimi <lidsheq97@gmail.com>
2023-06-11 18:32:40 +0200

    filter facilities by type

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import MapComponent from './components/MapComponent';
+import Sidebar from './components/Sidebar';
 import 'leaflet/dist/leaflet.css';
-import './App.scss'
+import './styles/App.scss';
 
 function App() {
-  
+  const [searchFilterValues, setSearchFilterValues] = useState<string[]>([]);
+
+  function getSearchValues(valuesArray: string[]) {
+    setSearchFilterValues(valuesArray);
+  }
 
   return (
     <div className="App">
-      <MapComponent />
+      <Sidebar handleSearch={getSearchValues} />
+      <MapComponent searchValues={searchFilterValues} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,34 +1,43 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import locations from "../locations.json"
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import locations from '../locations.json';
 import 'leaflet/dist/leaflet.css';
 
-function MapComponent() {
+type MapComponentProps = {
+  searchValues: string[];
+};
 
-    const markers = locations.map(location => {
+function MapComponent(props: MapComponentProps) {
+  const filteredPlaces = locations.filter((location) => {
+    return props.searchValues.includes(location.type);
+  });
 
-        const latLngs : [number, number] = [location.coordinates[0], location.coordinates[1]]
-        
-        return ( 
-        <Marker key={location.id} position={latLngs}>
-            <Popup className='popup'>
-                <h2 className='popup__title'>{location.name}</h2>
-                <p className='popup__phoneNumber'>{location.phoneNumber}</p>
-            </Popup>
-        </Marker>
-        )
-    })
+  const facilitiesMarkers = filteredPlaces.map((location) => {
+    const latLngs: [number, number] = [
+      location.coordinates[0],
+      location.coordinates[1],
+    ];
 
-    return( 
-        <div className='map'>
-            <MapContainer center={[50.04, 19.94]} zoom={13} scrollWheelZoom={true}>
-                <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {markers}
-            </MapContainer>
-        </div>
-    )
+    return (
+      <Marker key={location.id} position={latLngs}>
+        <Popup className="popup">
+          <h2 className="popup__title">{location.name}</h2>
+          <p className="popup__phoneNumber">{location.phoneNumber}</p>
+        </Popup>
+      </Marker>
+    );
+  });
+
+  return (
+    <div className="map">
+      <MapContainer center={[50.04, 19.94]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {facilitiesMarkers}
+      </MapContainer>
+    </div>
+  );
 }
 
-export default MapComponent
+export default MapComponent;
