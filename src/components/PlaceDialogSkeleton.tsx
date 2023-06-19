48a494e47822cb9234362e8d122627d75d640b7e
lidimi <lidsheq97@gmail.com>
2023-06-19 19:26:39 +0200

    placeDialog Skeleton

--- src/components/PlaceDialogSkeleton.tsx
+++ src/components/PlaceDialogSkeleton.tsx
@@ -1,9 +1,20 @@
-import Skeleton from '@mui/material/Skeleton';
+import { Skeleton, Stack } from '@mui/material/';
 
 function PlaceDialogSkeleton() {
   return (
     <div className="dialog-popup">
-      <Skeleton variant="rectangular" width={300} height={150} />
+      <div className="dialog-popup__photo">
+        <Skeleton variant="rectangular" width={300} height={150} />
+      </div>
+      <div className="dialog-popup__data-display">
+        <Skeleton variant="text" width={300} height={30} />
+        <Skeleton variant="text" width={300} height={30} />
+        <Skeleton variant="text" width={300} height={30} />
+      </div>
+      <div className="dialog-popup__user">
+        <Skeleton variant="text" width={400} height={30} />
+        <Skeleton variant="text" width={100} height={30} />
+      </div>
     </div>
   );
 }
